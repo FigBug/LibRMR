b4062af76f309b7fceafa86ce630bd35430935c3
Roland Rabien <figbug@gmail.com>
2013-09-03 15:05:42 -0700

    Adding Archive Reader / Archive Writer

---
+++ source/util/RMRArchiveReader.h
@@ -0,0 +1,19 @@
+//
+//  RMRArchiveReader.h
+//  LibRMR
+//
+//  Created by Roland Rabien on 2013-09-03.
+//  Copyright (c) 2013 Roland Rabien. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RMRArchiveReader : NSObject {
+    NSString* srcPath;
+}
+
++ (RMRArchiveReader*)archiveWriterForPath:(NSString*)path;
+
+- (BOOL)extractToPath:(NSString*)dstPath;
+
+@end
---
+++ source/util/RMRArchiveWriter.h
@@ -0,0 +1,24 @@
+//
+//  RMRArchive.h
+//  LibRMR
+//
+//  Created by Roland Rabien on 2013-09-03.
+//  Copyright (c) 2013 Roland Rabien. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RMRArchiveWriter : NSObject {
+    NSMutableArray* srcFiles;
+    NSMutableArray* dstFiles;
+    NSString* path;
+}
+
++ (RMRArchiveWriter*)archiveWriterForPath:(NSString*)path;
+
+- (void)addFileWithSourcePath:(NSString*)src;
+- (void)addFileWithSourcePath:(NSString*)src destinationPath:(NSString*)dst;
+
+- (BOOL)write;
+
+@end
