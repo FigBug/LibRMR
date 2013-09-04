46db67ee89afa046ca4503121fb71d99e88a1b2f
Roland Rabien <figbug@gmail.com>
2013-09-04 09:32:03 -0700

    Added butterworth filter

---
+++ source/util/RMRButterworthFilter.h
@@ -0,0 +1,22 @@
+//
+//  RMRButterworthFilter.h
+//  LibRMR
+//
+//  Created by Roland Rabien on 2013-09-04.
+//  Copyright (c) 2013 Roland Rabien. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RMRButterworthFilter : NSObject {
+    double xv[3];
+    double yv[3];
+    
+    double ax[3];
+    double by[3];
+}
+
+- (void)initWithSampleRate:(int)rate cuttoffFrequnecy:(double)freq;
+- (double)filter:(double)val;
+
+@end
